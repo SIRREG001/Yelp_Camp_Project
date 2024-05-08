5a3c94bf98424d482ed9ea87ed112ca5077d8dc0
SIRREG001 <udochukwureginald78@gmail.com>
2024-05-08 08:34:21 +0100

    Setting up sessions using express-session

--- app.js
+++ app.js
@@ -34,15 +34,17 @@ app.use(methodOverride('_method'));
 //serving public files
 app.use(express.static(path.join(__dirname, 'public')))
 
-// const sessionConfig = {
-//     secret: 'thisshouldbesecret',
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {
-//         expires: Date.now() + 
-//     }
-// }
-// app.use(session(sessionConfig))
+const sessionConfig = {
+    secret: 'thisshouldbesecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60* 24 * 7,
+        maxAge: 1000 * 60 * 60* 24 * 7
+    }
+}
+app.use(session(sessionConfig))
 
 
 
