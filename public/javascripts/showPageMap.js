2a0d867b0885a6ea110b81316b5e64003cf4d92d
SIRREG001 <udochukwureginald78@gmail.com>
2024-06-23 21:42:57 +0100

    Adding styles to map and popup features

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,12 +1,13 @@
 mapboxgl.accessToken = mapToken;
-const obj = JSON.parse(campground)
 const map = new mapboxgl.Map({
     container: 'map',
-    center: obj.geometry.coordinates,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: campground.geometry.coordinates,
     zoom: 9
 });
-
 new mapboxgl.Marker()
-    .setLngLat([obj.geometry.coordinates])
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({offset: 25})
+    .setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`))
     .addTo(map)
 
